f9b69b94d74438ae6f2d15d558b29cb87e5f8c3e
Deepak Singh <deepakanil@gmail.com>
2020-05-22 14:13:11 +0200

    Fixed VM argument issue

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfigurati
 @SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerInventoryApplication {
 
-    public static void main(String[] args) {
+    public static void main(String ...args) {
         SpringApplication.run(BeerInventoryApplication.class, args);
     }
 
